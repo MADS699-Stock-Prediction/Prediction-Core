d3f55fd41e85a5b30919a530311709ca6c4176d8
Atul Kumar <atullh@gmail.com>
2023-07-13 21:44:21 +0530

    Included toy sentiment analyzer for news data

---
+++ src/sentiment_analyzer/analyze_n_append_gnews_sentiment.py
@@ -0,0 +1,58 @@
+#pip install -q transformers
+from transformers import pipeline
+import pandas as pd
+
+def senttiment_to_number(x):
+    if x =='NEU':
+        return 0
+    if x =='POS':
+        return 1
+    else:
+        return -1
+    
+def extract_gnews_sentiment(stock_id =['TSLA'], start_date ='02/01/2020', 
+                            end_date ='02/01/2022',
+                                 raw_tech_data_store_dir='data/googlenewsdata/',
+                                 clean_tech_data_store_dir='data/googlenewsdata/'
+                                 ):
+    print(stock_id,raw_tech_data_store_dir)
+
+    for id in list(stock_id):
+        print(id)
+        #gnews_data =pd.DataFrame(data)
+        file_path_pygnews = raw_tech_data_store_dir + "/googlenewsdata_pygnews_" + id + "_"+start_date +"_" +end_date
+        file_path_gnews = raw_tech_data_store_dir + "/googlenewsdata_gnews_" + id + "_"+start_date +"_" +end_date
+# as of now sentiment from pygooglenews only
+        sentiment_pipeline = pipeline(model="finiteautomata/bertweet-base-sentiment-analysis")
+        df = pd.read_csv(file_path_pygnews)
+        data = list(df.title)
+        df['sentiment'] = pd.DataFrame(sentiment_pipeline(data))['label']
+        df['sentiment'] = df['sentiment'].apply(senttiment_to_number)
+        file_path_gnews_with_sentiment = clean_tech_data_store_dir + "/googlenewsdata_gnews_" + id + "_"+start_date +"_" +end_date
+
+        print(file_path_gnews_with_sentiment)
+        df.to_csv(file_path_gnews_with_sentiment)
+
+
+if __name__ == '__main__':
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--stock_ids",
+        '--names-list',
+        nargs="*",  
+        default=['TSLA'],
+        )
+    parser.add_argument('-raw_tech_data_store_dir', help='directory to append sentiment with raw data')
+    parser.add_argument('-clean_tech_data_store_dir', help='directory to store clean technical data files')
+
+    parser.add_argument('-start_date', help='start date information')
+    parser.add_argument('-end_date', help='end_date information')
+    args = parser.parse_args()
+    print(args)
+    extract_gnews_sentiment(stock_id=args.stock_ids, 
+                            raw_tech_data_store_dir =args.raw_tech_data_store_dir,
+                            clean_tech_data_store_dir= args.clean_tech_data_store_dir,
+                            start_date = args.start_date,
+                                end_date = args.end_date)
