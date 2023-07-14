f1dce9fd926c13d671f9d41ddf44dfef0a73da01
Atul Kumar <atullh@gmail.com>
2023-07-14 08:11:32 +0530

    Added pycaret generic model

--- src/model_factory/pcaret_ts_model.py
+++ src/model_factory/pcaret_ts_model.py
@@ -1,7 +1,8 @@
 # can not install on the local system works on colab only
 from pycaret.time_series import *
+from pycaret import *
 import pandas as pd
-
+import pickle
 
 def get_best_ts_model(stock_id =['TSLA'], 
                start_date ='2008-03-01', 
@@ -9,20 +10,25 @@ def get_best_ts_model(stock_id =['TSLA'],
                clean_tech_data_store_dir='data/clean_data',
                model_storage_path = 'models/'):
     for id in stock_id:
-        clean_file_path = clean_tech_data_store_dir + "/tech_indicator_pygooglenews" + id + "_"+start_date +"_" +end_date
-
+        clean_file_path = clean_tech_data_store_dir + "/tech_indicator_" + id + "_"+start_date +"_" +end_date
+    print(clean_file_path)
     df = pd.read_csv(clean_file_path)
-    df = df.asfreq('B')
-    s = setup(data =df.Close , fh = 3, fold = 5, session_id = 123, numeric_imputation_target ='ffill')
+    df = df[['Close']]
+    print(df.head())
+    #df = df.asfreq('B')
+    print(df.head())
+    df = df[['Close']]
+    print(df.head())
+    s = setup(data =df.Close , fh = 3, fold = 5, session_id = 123, numeric_imputation_target ='ffill', log_experiment=True)
     best = compare_models()
     plot_model(best, plot = 'forecast', data_kwargs = {'fh' : 24})
     plot_model(best, plot = 'diagnostics')
     plot_model(best, plot = 'insample')
     final_best = finalize_model(best)
     predict_model(best, fh = 24)
-    with open(model_output_path, 'wb+') as out:
+    model_storage_path += "pycaret_model.pkl"
+    with open(model_storage_path, 'wb+') as out:
         pickle.dump(final_best, out)
-
 
 if __name__ == '__main__':
     import argparse
@@ -34,13 +40,15 @@ if __name__ == '__main__':
         nargs="*",  
         default=['TSLA'],
         )
-    parser.add_argument('-raw_tech_data_store_dir', help='directory to store raw technical data files')
+    parser.add_argument('-clean_tech_data_store_dir', help='directory to store raw technical data files')
     parser.add_argument('-start_date', help='start date information')
     parser.add_argument('-end_date', help='end_date information')
-    args = parser.parse_args()
+    parser.add_argument('-model_storage_path', help='directory to store model in pickel format')
 
+    args = parser.parse_args()
+    print(args)
     get_best_ts_model(stock_id=args.stock_ids, 
-                      raw_tech_data_store_dir =args.raw_tech_data_store_dir,
+                      clean_tech_data_store_dir= args.clean_tech_data_store_dir,
                         model_storage_path = args.model_storage_path, 
                         start_date = args.start_date,
                         end_date = args.end_date)
